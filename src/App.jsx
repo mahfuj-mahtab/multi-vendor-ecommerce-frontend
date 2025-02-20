27ba89e96e43f6adb00fb6afca44f6f9c4e27a74
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2025-02-20 11:13:31 +0600

    working on banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
+import Header from './components/Header'
+import Carousel from './components/Carousel'
+import MyCarousel from './components/Carousel'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-   
-
+      <Header/>
+    <MyCarousel/>
     </div>
   )
 }
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
+  
+function MyCarousel() {
+  return (
+    <div className='container mx-auto max-w-7xl mt-5 h-[500px]'>
+    <Carousel>
+        <CarouselContent>
+            <CarouselItem>
+                <div  className='h-[500px] border-2'>
+                    <h1>This is banner</h1>
+                </div>
+            </CarouselItem>
+            <CarouselItem>
+
+            </CarouselItem>
+            <CarouselItem>
+
+            </CarouselItem>
+        </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+    </div>
+
+  )
+}
+
+export default MyCarousel
