4ff933f3a91a2bafae273bcde0ab0d337280dc66
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2025-03-03 21:06:07 +0600

    react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
+import SingleProduct from './components/SingleProduct.jsx';
 
+    
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/single" element={<SingleProduct />} />
+        </Routes>
+      
+
+    </BrowserRouter>
+    
+  
 )
