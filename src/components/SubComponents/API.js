74c3491766772bf2d82aa11307fef93f9ed71486
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2025-03-15 12:23:22 +0600

    api intercept fixed

--- src/components/SubComponents/API.js
+++ src/components/SubComponents/API.js
@@ -1,42 +1,55 @@
 import axios from "axios";
+
 export const api = 'http://127.0.0.1:8000';
 
 const API = axios.create({
-    baseURL : api,
-    headers : {
-        'Content-Type' : 'application/json'
+    baseURL: api,
+    headers: {
+        'Content-Type': 'application/json'
     },
-    withCredentials : true
-})
-API.interceptors.request.use(async (config)=>{
-    let token = localStorage.getItem('accessToken')
-    if(token){
-        config.headers.Authorization = `Bearer ${token}`
+    withCredentials: true
+});
+
+
+API.interceptors.request.use(async (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
-    return config
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
 
-},
-(error)=>{
-    return Promise.reject(error)
-})
 
-API.interceptors.response.use(async (response)=>{
-    return response
-},
-async (error)=>{
-    if(error.response.status === 403){
-        
-        try {
-            const {data} = await axios.get('/api/v1/users/refresh-token')
-            localStorage.setItem('accessToken', data.accessToken)
-            error.config.headers.Authorization = `Bearer ${data.accessToken}`
-            return API(error.config)
-        } catch (error) {
-            localStorage.removeItem('accessToken')
-            window.location.href = '/login'
+API.interceptors.response.use(
+    (response) => response, 
+    async (error) => {
+        const originalRequest = error.config;
+
+       
+        if (error.response && (error.response.status === 403 || error.response.status === 401)) {
+            try {
+             
+                const { data } = await axios.get(`${api}/api/v1/users/refresh-token`, { withCredentials: true });
+
+                
+                localStorage.setItem('accessToken', data.accessToken);
+
+           
+                originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
+                return API(originalRequest);
+            } catch (refreshError) {
+                console.error("Refresh token expired or invalid:", refreshError);
+
+                
+                localStorage.removeItem('accessToken');
+                window.location.href = '/login';
+            }
         }
+
+        return Promise.reject(error);
     }
-   
-    return Promise.reject(error)
-})
-export default API
+);
+
+export default API;
